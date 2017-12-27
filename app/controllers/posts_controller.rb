2dc8c88d0614d97ce49b769a27432fd14bbd903e
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-27 23:27:33 +0530

    New Action Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+
   end
 
   def index
+
   end
 
   def show
+
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+
   end
 end

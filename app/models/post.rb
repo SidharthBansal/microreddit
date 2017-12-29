b307381b8c84a6b347bb559f88d78cecbb950231
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 14:17:55 +0530

    Added more validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
-  validates :title, length: {maximum: 50, minimum: 10}
+  validates :title, length: {maximum: 50, minimum: 10},
+                    presence: true
+  validates :description, length: {maximum: 500, minimum: 20},
+                                      presence: true
   belongs_to :user
 end

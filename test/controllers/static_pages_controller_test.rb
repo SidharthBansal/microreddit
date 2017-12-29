3535c6ba2a7525acec1f9f0fcb6df39b329f37fa
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 14:50:56 +0530

    Modified test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Microreddit"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Microreddit"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Microreddit"
   end
 
 end

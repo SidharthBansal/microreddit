43b88a25f8d9f8bf67b2ac6b51c458c1de4ff722
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 22:32:37 +0530

    Added Sign Up Integration Test

--- test/integration/users_sign_up_test.rb
+++ test/integration/users_sign_up_test.rb
@@ -4,4 +4,17 @@ class UsersSignUpTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {name: "",
+                                        email: "user@invallid",
+                                        password: "foo",
+                                        password_confirmation: "foo"}}
+    end
+    assert_template 'users/new'
+  end
+
+
+
 end

c3ca3af4a2dfe9e89c780eed06705a988ea48de7
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 14:01:26 +0530

    Added root' '

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,6 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:title, :desscription)
+      params.require(:post).permit(:title, :description)
     end
 end

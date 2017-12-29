2121ceeda9d70ab2dab16adaa34fca0dae796f00
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 18:44:58 +0530

    Added user length model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   validates :name, presence: true
   validates :email, presence: true
+  validates :name,  presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }
 
 end

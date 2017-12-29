345f5f6fc18ce61497ad421ca4ce2c125ffadc13
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 22:25:22 +0530

    users#create added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
+   @user = User.new(user_params)
+   if @user.save
+     # Handle a successful save.
+   else
+     render 'new'
+   end
   end
 
   def index
@@ -21,4 +27,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  private
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end

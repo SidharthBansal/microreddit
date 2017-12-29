dbf3226fcc75e6bfb89b9ebb52017f50f80e0e7d
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 18:36:41 +0530

    Added Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227125117) do
+ActiveRecord::Schema.define(version: 20171229125733) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171227125117) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end

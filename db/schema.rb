c4aabe9a32d085a71aaae2f48e835c9bf09f14ed
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 22:48:58 +0530

    Added integration test valid sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171229144808) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- test/integration/users_sign_up_test.rb
+++ test/integration/users_sign_up_test.rb
@@ -15,6 +15,17 @@ class UsersSignUpTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
+  test "valid sign_up" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {name: "Example User",
+                                        email: "user@example.com",
+                                        password: "password",
+                                        password_confirmation: "password"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 
 
 end

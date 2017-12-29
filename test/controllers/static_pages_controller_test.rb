3def782d4fa241a03f850c736306afecee4e560b
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-29 15:03:39 +0530

    Modified static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Microreddit"
   end
 
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success

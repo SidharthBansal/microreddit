94533c3469b232d60ad31bf96f258dc471aab0c0
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-31 02:50:12 +0530

    Added fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   has_many :posts
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -32,5 +34,18 @@ class User < ApplicationRecord
     end
     def forget
       update_attribute(:remember_digest, nil)
+    end
+
+    private
+
+    # Converts to lower case
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Create and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end

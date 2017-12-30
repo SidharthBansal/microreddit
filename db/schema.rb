785c37ebffb049091d71b2d133911a243bbed14c
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-31 02:17:00 +0530

    Account activation controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230181854) do
+ActiveRecord::Schema.define(version: 20171230204120) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"

bddd133f9527e1c7c1b761e55a8a5c1617e59286
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-30 12:30:28 +0530

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
   resources :users
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
+
+  get  '/login',  to: 'sessions#new'
+  post  '/login',  to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

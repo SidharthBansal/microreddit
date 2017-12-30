597d84d15e2d25263094552c9f3ad1782e96ee94
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-30 22:28:58 +0530

    Added correct_user callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -20,11 +21,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(params[:id])
   end
 
   def update
-      @user = User.find(params[:id])
       if @user.update_attributes(user_params)
         flash[:success] = "Profile updated"
         redirect_to @user
@@ -39,11 +38,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
+# Before filter
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in"
         redirect_to login_url
       end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end

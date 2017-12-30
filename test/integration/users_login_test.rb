bc0adaee4b2db0a481f34c209c9f4cbade918545
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-30 13:07:25 +0530

    Added login tests

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid log in" do
+    get login_path
+    assert_template "sessions/new"
+    assert flash.empty?
+    post login_path, params: { session: {email: "", password: ""}}
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end

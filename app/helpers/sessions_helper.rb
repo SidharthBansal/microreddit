3ae3479d39996956d3d63265c3997368347854e5
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-30 22:31:03 +0530

    Added current_user?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
   def current_user
     if(user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)

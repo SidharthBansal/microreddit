6c1c06214a0c7239b080e0e4098e4ae371da7022
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-31 22:37:46 +0530

    Added post controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,13 +50,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 # Before filter
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-    end
+  #present in the ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)

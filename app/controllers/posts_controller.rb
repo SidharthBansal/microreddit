26f6be85ae3ac74a14804455edba8329ba791b2a
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-31 23:56:13 +0530

    Added destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @post = current_user.posts.build(post_params)
@@ -18,11 +19,14 @@ class PostsController < ApplicationController
 
   def edit
   end
+
   def update
   end
 
   def destroy
-
+    @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
@@ -30,4 +34,10 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :description)
     end
+
+    def correct_user
+      @Post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
+
 end

a420543760543f658ac9916bae725e452ea276cb
Sidharth <bansal.sidharth2996@gmail.com>
2018-01-02 01:29:27 +0530

    Added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
-
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
   has_many :posts, dependent: :destroy
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

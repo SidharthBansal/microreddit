182c092cef495f9cb4919f08c74ca13804b6c44f
Sidharth <bansal.sidharth2996@gmail.com>
2018-01-06 16:58:18 +0530

    fixed indentation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,26 +20,27 @@ class UserTest < ActiveSupport::TestCase
   test "email should be present" do
      @user.email = "     "
      assert_not @user.valid?
-   end
-   test "name should not be too long" do
-       @user.name = "a" * 51
-       assert_not @user.valid?
-     end
+  end
+  test "name should not be too long" do
+     @user.name = "a" * 51
+     assert_not @user.valid?
+  end
 
-     test "email should not be too long" do
-       @user.email = "a" * 244 + "@example.com"
-       assert_not @user.valid?
-     end
-     test "email validation should accept valid addresses" do
-       valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                            first.last@foo.jp alice+bob@baz.cn]
-       valid_addresses.each do |valid_address|
-         @user.email = valid_address
-         assert @user.valid?, "#{valid_address.inspect} should be valid"
-       end
-     end
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 
-     test "email validation should reject invalid addresses" do
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                          first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -47,69 +48,76 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
+
   test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+
   test "password should be present (nonblank)" do
       @user.password = @user.password_confirmation = " " * 6
       assert_not @user.valid?
-    end
-
-    test "password should have a minimum length" do
-      @user.password = @user.password_confirmation = "a" * 5
-      assert_not @user.valid?
-    end
-
-    test "authenticated? should return false for a user with nil digest" do
-      assert_not @user.authenticated?(:remember, '')
-    end
-
-    test "order should be most recent first" do
-      assert_equal posts(:most_recent), Post.first
-    end
-
-    test "associated posts should be destroyed" do
-      @user.save
-      @user.posts.create!(title: "this is the title ",
-                          description: "this is the description of the first post. Hope you have liked the post")
-     assert_difference 'Post.count', -1 do
-       @user.destroy
-     end
-    end
-    test "should follow and unfollow a user" do
-      michael = users(:michael)
-      archer  = users(:archer)
-      assert_not michael.following?(archer)
-      michael.follow(archer)
-      assert michael.following?(archer)
-      assert archer.followers.include?(michael)
-      michael.unfollow(archer)
-      assert_not michael.following?(archer)
-    end
-    test "feed should have the right posts" do
-  michael = users(:michael)
-  archer  = users(:archer)
-  lana    = users(:lana)
-  # Posts from followed user
-  lana.posts.each do |post_following|
-    assert michael.feed.include?(post_following)
   end
-  # Posts from self
-  michael.posts.each do |post_self|
-    assert michael.feed.include?(post_self)
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
-  # Posts from unfollowed user
-  archer.posts.each do |post_unfollowed|
-    assert_not michael.feed.include?(post_unfollowed)
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
   end
-end
+
+  test "order should be most recent first" do
+    assert_equal posts(:most_recent), Post.first
+  end
+
+  test "associated posts should be destroyed" do
+    @user.save
+    @user.posts.create!(title: "this is the title ",
+                        description: "this is the description of the first post. Hope you have liked the post")
+    assert_difference 'Post.count', -1 do
+      @user.destroy
+    end
+  end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.posts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.posts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.posts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
+  
 end

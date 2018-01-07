cf3644a504a43d5712d616114674b6a1f5e3d8d4
Sidharth <bansal.sidharth2996@gmail.com>
2018-01-07 13:42:52 +0530

    Modified

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,11 +30,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
      assert_select "a[href=?]", user_path(@user), count: 0
    end
 
-   test "login with remembering" do
-     log_in_as(@user, remember_me: '1')
-     assert_not_empty cookies['remember_token']
-   end
-
    test "login without remembering" do
      # Log in to set the cookie.
      log_in_as(@user, remember_me: '1')
@@ -42,4 +37,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
      log_in_as(@user, remember_me: '0')
      assert_empty cookies['remember_token']
    end
+
+   test "login with remembering" do
+   log_in_as(@user, remember_me: '1')
+   assert_equal assigns(:user).remember_token, cookies['remember_token']
+ end
  end
